3962370ea03b95c90c68d185faa9e837cbcfb4ca
Joey <joseph.d.brown94@gmail.com>
2020-12-10 14:05:13 -0500

    Updated files w/instructions

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+Fizz Buzz is a traditional coding interview challenge done on a white board. In the past, companies would have potential employees
+write out the solution by hand. Since we can not do that here, you will be typing the solution on VSCode.
+ 
+Your goal is to loop through 20 numbers and return the following
+   * If the number is divisible by 3 (3, 6, 9) then console log the word Fizz
+   * If the number is divisible by 5 (5, 10, 15) then console log the word Buzz
+   * If the number is divisible by both 3 AND 5 then console log the word FizzBuzz
+   * If the number is NOT divisible by 3 and/or 5 then console log the number itself
+ 
+Stretch Goals!!!!!
+   * Instead of using console log display the words/numbers on the body of the page in an unordered list
+   * Allow users to choose how many numbers to loop through and display the results on the page
+   * Maybe add a reset button
+   * Figure out a way to get the same results WITHOUT using any loops. (Yes there is a way, try to research it)
+*/
